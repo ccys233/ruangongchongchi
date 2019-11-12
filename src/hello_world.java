29cea8b36d0d6a8eef1e2ffb676c56121cdac96a
ccys233 <54995020+ccys233@users.noreply.github.com>
2019-11-12 22:07:52 +0800

    1.0
    1.0

---
+++ src/hello_world.java
@@ -0,0 +1,20 @@
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+ 
+
+public class hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello world");
+	}
+
+}
